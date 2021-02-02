25fb5e7b78b2b4b5c5b483be2d0c68a5f8f57290
samleemcknight <samleemcknight@gmail.com>
2021-02-01 17:18:18 -0700

    changed styles and switched to Recipe - food - nutrition API

--- js/script.js
+++ js/script.js
@@ -3,26 +3,37 @@ console.log('sanity cherk')
 const textBar = document.getElementById("text-bar")
 const button = document.querySelector("button")
 const pantryList = document.querySelector("ul")
-const images = document.getElementById("image-container")
+const images = document.getElementById("recipe-container")
 const submit = document.getElementById("submit")
 
 //currently defined here to help with console.logging
 let elEyes = null
 let recipes = null
 
+let ingredientList = []
+
 const imageCreator = (recipes) => {
     for (let i = 0; i < recipes.length; i++) {
-        if (typeof recipes[i].thumbnail !== "undefined") {
-            let img = document.createElement("img")
-            img.setAttribute("src", recipes[i].thumbnail)
-            img.setAttribute("alt", recipes[i].title)
-            images.appendChild(img)
-            let a = document.createElement("a")
-            a.textContent = img.getAttribute("alt")
-            a.setAttribute("href", recipes[i].href)
-            a.setAttribute("target", "_blank")
-            images.appendChild(a)
-        }
+        let img = document.createElement("img")
+        img.setAttribute("src", `https://spoonacular.com/recipeImages/${recipes[i].image}`)
+        img.setAttribute("alt", recipes[i].title)
+
+        let a = document.createElement("a")
+        a.textContent = img.getAttribute("alt")
+        a.setAttribute("href", recipes[i].sourceUrl)
+        a.setAttribute("target", "_blank")
+        images.appendChild(a)
+
+        a.appendChild(img)
+
+        if (document.querySelectorAll('img').length === 6) { break }
+    }
+}
+
+const randomizer = (arr) => {
+    for (let i = arr.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
     }
 }
 
@@ -32,20 +43,29 @@ button.addEventListener("click", (evt) => {
     li.textContent = textBar.value
     pantryList.appendChild(li)
     elEyes = document.querySelectorAll('li')
+    textBar.value = ''
+    ingredientList.push(elEyes[elEyes.length - 1].innerText)
+    randomizer(ingredientList)
 })
 
 submit.addEventListener("click", (evt) => {
     evt.preventDefault()
-    let ingredientList = []
-    for (let i = 0; i < elEyes.length; i++) {
-        ingredientList.push(elEyes[i].innerText)
-    }
-    fetch(`http://www.recipepuppy.com/api/?i=${ingredientList[0]},${ingredientList[1]},${ingredientList[2]}`)
+    // resets images in case 
+    images.innerHTML = ''
+    fetch(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query=${ingredientList[0]}%2C${ingredientList[1]}%2C${ingredientList[2]}`,
+    {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-key": "37daf3cd7cmshd6e976bb1dbe4dep11f211jsn70154af64eb3",
+                "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+            }
+        })
         .then((responseData) => {
             return responseData.json()
         })
         .then((jsonData) => {
             recipes = jsonData.results
+            randomizer(recipes)
             imageCreator(recipes)
         })
 })
